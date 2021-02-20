d0cf317fe7bf900d8b9385e9387b6ac1980d56bf
Qing Ma <gsmofgsm@hotmail.com>
2021-02-20 21:22:32 +0100

    Get restaurants data in Page Model

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
 using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
@@ -12,19 +13,21 @@ namespace OdeToFood.Pages.Restaurants
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
-        private readonly IRestaurantData restaurant;
+        private readonly IRestaurantData restaurantData;
 
         public string Message { get; set; }  // to access in the page via @Model.Message
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
-        public ListModel(IConfiguration config, IRestaurantData restaurant)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
-            this.restaurant = restaurant;
+            this.restaurantData = restaurantData;
         }
 
         public void OnGet()
         {
             Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
