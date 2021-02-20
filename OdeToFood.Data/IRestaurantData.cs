8a715d0eeb03e115299192370e3d872e4e2669af
Qing Ma <gsmofgsm@hotmail.com>
2021-02-20 21:10:38 +0100

    Data access service programmed to an interface following dependency inversion pattern

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,37 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        // .net core follows dependency inversion pattern
+        // we program to an interface
+        // to allow smooth switching to database later
+        IEnumerable<Restaurant> GetAll();
+    }
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+                {
+                    new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
+                    new Restaurant { Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Indian},
+                    new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.None},
+                };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
