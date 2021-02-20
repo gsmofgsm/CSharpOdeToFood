674f8a10c02883b95c27badf523e2386f0474506
Qing Ma <gsmofgsm@hotmail.com>
2021-02-20 16:09:04 +0100

    Using configuration to inject Message from appsettings

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,16 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+
         public string Message { get; set; }  // to access in the page via @Model.Message
+
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
 
         public void OnGet()
         {
-            Message = "Hello, World!";
+            Message = config["Message"];
         }
     }
 }
