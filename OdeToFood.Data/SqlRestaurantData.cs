6b8828418970c05c3b171449b657d6dca5d91fdd
Qing Ma <gsmofgsm@hotmail.com>
2021-02-21 10:51:24 +0100

    another implementation SqlRestaurantData

---
+++ OdeToFood.Data/SqlRestaurantData.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            db.Add(newRestaurant);
+            return newRestaurant;
+        }
+
+        public int Commit()
+        {
+            return db.SaveChanges();
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = GetById(id);
+            if (restaurant != null)
+            {
+                db.Restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return db.Restaurants.Find(id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            var query = from r in db.Restaurants
+                        where r.Name.StartsWith(name) || string.IsNullOrEmpty(name)
+                        orderby r.Name
+                        select r;
+            return query;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            // Attach means:
+            // I am about to give you an object,
+            // that represents information that is already in the database
+            // I want you to start tracking changes about this object
+            var entity = db.Restaurants.Attach(updatedRestaurant);
+            entity.State = EntityState.Modified;
+            // entityframework will assume that everything about this object has changed
+            // except for Id
+            // and they will be updated in de db when .SaveChanges()
+            return updatedRestaurant;
+        }
+    }
+}
